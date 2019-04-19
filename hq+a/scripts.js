e19165205313db48a1b795bf9ead69c4d19ffcbc
Lord Vollkorn <bertozzi@i-ways.de>
2019-04-19 21:00:46 +0200

    official ost playlist with 6 tracks

--- hq+a/scripts.js
+++ hq+a/scripts.js
@@ -34,8 +34,45 @@ function init ()
 	
     $(function ()
     {
-    	var audio_player = document.getElementById('audio-player');
+    	var audio_player = document.getElementById('audio-player'),
+    	    audio_tracks = $('#playlist a'),
+    	    audio_loop = true,
+    	    audio_loop_control = $('#playlist #player-loop'),
+    	    audio_stop_control = $('#playlist #player-stop');
     	
+    	audio_player.load()
     	audio_player.play(); // not working because of policies..
+    	
+    	audio_loop_control.on('click', function ()
+		{
+    		audio_loop = !audio_loop;
+    		$(this).toggleClass('active');
+		});
+    	
+    	audio_stop_control.on('click', function ()
+		{
+    		audio_player.pause();
+    		audio_player.currentTime = 0;
+    		audio_tracks.removeClass('active');
+		});
+    	
+    	audio_tracks.on('click', function (e)
+        {
+    		audio_tracks.removeClass('active');
+    		e.preventDefault();
+    		audio_player.src = 'ost/' + $(this).prop('rel') + '.mp3';
+    		audio_player.play();
+    		$(this).addClass('active');
+    	});
+    	
+    	audio_player.onended = function ()
+    	{
+    		if (audio_loop) {
+    			audio_player.currentTime = 0;
+    			audio_player.play();
+    		} else {
+    			audio_tracks.removeClass('active');
+    		}
+    	};
     });
 }
