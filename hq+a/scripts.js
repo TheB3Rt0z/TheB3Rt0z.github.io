e9b27e417d2f5f209919c2ce5d27fe19d23a6378
Lord Vollkorn <bertozzi@i-ways.de>
2019-08-08 13:24:01 +0200

    dice launcher ahoy

--- hq+a/scripts.js
+++ hq+a/scripts.js
@@ -49,6 +49,11 @@ function init ()
     	    status_controller = $('#status-controller'),
     	    status_controller_import = status_controller.find('#status-import'),
     	    status_controller_export = status_controller.find('#status-export'),
+    	    move_amount = $('#dice-move-amount'),
+    	    move_button = $('#dice-move'),
+    	    attack_amount = $('#dice-attack-amount'),
+    	    attack_button = $('#dice-attack'),
+    	    dice_result = $('#dice-result'),
     	    reset_button = $('#reset-data'),
     	    utility_textarea = $('#footer ~ textarea');
     	
@@ -63,6 +68,7 @@ function init ()
     			clearInterval(auto_save);
     			if (confirm('Are you SURE?')) {
     				window.hqa_data.status = 'preparation';
+    				dice_result.html('');
     			} else {
     				start_autosave();
     				return;
@@ -87,6 +93,45 @@ function init ()
     		e.stopPropagation();
     		utility_textarea.val(JSON.stringify(window.hqa_data)).show(DEFAULT_DELAY).select();
 		});
+    	
+    	move_button.on('click', function (e)
+    	{
+    		var dices = [];
+    		
+    		dice_result.html('<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>');
+    		
+    		for (i = 0; i < move_amount.val(); i++) {
+    			dices.push(Math.floor((Math.random() * 6) + 1));
+    		}
+    		
+    		setTimeout(function () {
+    			dice_result.html(dices.join(" + ") + " = " + dices.reduce((pv, cv) => pv + cv, 0));
+    		}, DEFAULT_DELAY * 3);
+    	});
+    	
+    	attack_button.on('click', function (e)
+    	{
+    		var faces = [
+    		        'skull',
+    		        'skull',
+    		        'skull',
+    		        'white',
+    		        'white',
+    		        'black'
+    		    ],
+    		    output = '';
+    		
+    		dice_result.html('<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>');
+    		
+    		
+    		for (i = 0; i < attack_amount.val(); i++) {
+    			output += '&nbsp;<img src="img/dice_' + faces[Math.floor(Math.random() * 6)] + '.png" />';
+    		}
+    		
+    		setTimeout(function () {
+    			dice_result.html(output);
+    		}, DEFAULT_DELAY * 3);
+    	});
     	
     	reset_button.on('click', function () // only as debug
 		{
